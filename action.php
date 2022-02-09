cf47ee64a47ee0edaadb4a1eba06088d6c87b719
taroksaha04 <taroksaha04@gmail.com>
2022-02-09 13:04:18 +0600

    day18final

---
+++ action.php
@@ -0,0 +1,39 @@
+<?php
+require_once 'vendor/autoload.php';
+use App\classes\Home;
+use App\classes\Series;
+use App\classes\Register;
+if(isset($_GET['pages']))
+{
+    if($_GET['pages']=='home')
+    {
+        include 'pages/home.php';
+    }
+    elseif ($_GET['pages']=='series'){
+        include 'pages/series.php';
+    }
+    elseif($_GET['pages']=='register')
+    {
+        include 'pages/register.php';
+
+    }
+}
+elseif (isset($_POST['btn'])){
+     $home = new Home($_POST);
+     $result= $home->primeNumber();
+     include 'pages/home.php';
+}
+elseif (isset($_POST['series_btn'])){
+    $series= new Series($_POST);
+    $result= $series->getSeries();
+    include 'pages/series.php';
+
+}
+elseif ($_POST['register_btn']){
+    $register = new Register($_POST);
+}
+else
+{
+    $home = new Home();
+    $home ->index();
+}
---
+++ app/classes/Register.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace App\classes;
+class Register
+{
+    public function __construct($post = null)
+    {
+        echo '<pre>';
+        print_r($post);
+    }
+}
